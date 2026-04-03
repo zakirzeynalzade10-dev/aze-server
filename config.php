ce90ccf8244a80121652775f1bb62057597889c7
zakirzeynalzade10-dev <zakirzeynalzade10@gmail.com>
2026-04-03 23:52:23 +0400

    Delete config.php

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Bazaya ehtiyac yoxdur, məlumatlar faylda saxlanacaq
-define('DATA_FILE', 'data.json');
-
-function getDB() {
-    // Bu funksiya köhnə kodlarla uyğunluq üçün boş qalır
-    return null;
-}
-?>
